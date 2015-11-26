35a4694b84a713610290f42b7520813c0bf5940a
Gilson-Android <gilsonlig100@gmail.com>
2015-11-26 01:09:48 -0200

    ADD ACTIVITY DE SERVICOS

--- ApresentacaoAndroid/app/src/main/java/ggc/apresentaoandroid/PrincipalActivity.java
+++ ApresentacaoAndroid/app/src/main/java/ggc/apresentaoandroid/PrincipalActivity.java
@@ -1,11 +1,14 @@
 package ggc.apresentaoandroid;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
@@ -14,31 +17,24 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 
-import java.util.Locale;
-
-import ggc.apresentaoandroid.adapters.NavigationDrawerListAdapter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrincipalActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
-
-    private Toolbar mtoolbar;
-
-    private ActionBarDrawerToggle mDrawerToogle;
-
-    private ListView mListaDrawer;
+    NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,100 +51,63 @@ public class PrincipalActivity extends AppCompatActivity {
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        if (navigationView != null) {
-            setupDrawerContent(navigationView);
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (mNavigationView != null) {
+            setupDrawerContent(mNavigationView);
         }
 
-        // FragmentManager fragmentManager = getSupportFragmentManager();
-
-        /// SobreNosFragment fragment = SobreNosFragment.newInstance();
-
-        //fragmentManager.beginTransaction().replace(R.id.conteudo_layout, fragment).commit();
-
-        // inicializarToolbar();
-
-        //inicializarDrawer();
-    }
-
-    /*public void inicializarToolbar()
-    {
-        mtoolbar = (Toolbar) findViewById(R.id.toolbar);
-
-        //Define titulo do toolbar
-        mtoolbar.setTitle("Apresentação Syshouseapps");
-
-        //Define o icon do drawer no Toolbar
-        mtoolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_action_menu));
-
-        mtoolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
+            public boolean onNavigationItemSelected(MenuItem item) {
 
-                mDrawerLayout.openDrawer(mListaDrawer);
+                if(item.isChecked()){
+                    item.setChecked(false);
+                } else {
+                    item.setChecked(true);
+                }
+
+                mDrawerLayout.closeDrawers();
+
+                switch(item.getItemId()){
+                    case R.id.nav_home:
+                        Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_sobrenos:
+                        Toast.makeText(getApplicationContext(), "Sobreenos", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_servicos:
+                        Context context = mDrawerLayout.getContext();
+                        Intent intent = new Intent(context, ServicosActivity.class);
+                        context.startActivity(intent);
+                        return true;
+                    case R.id.nav_contato:
+                        Toast.makeText(getApplicationContext(), "Sites", Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
             }
         });
-    }*/
 
-
-   /* public void inicializarDrawer()
-    {
-
-        //Definir a posicao selecionada e a ultima do drawer
-        mPosicaoSelecionada = 1;
-
-        mUltimaPosicaoDoDrawer  = mPosicaoSelecionada;
-
-        //Pega o DrawerLayout
-        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
-
-        //Pega a lista
-        mListaDrawer = (ListView) findViewById(R.id.nav_drawer);
-
-
-        //Criar um toogler para abrir e fechar o drawer ao clicar no icon no toolbar
-        mDrawerToogle = new ActionBarDrawerToggle(this,mDrawerLayout,mtoolbar,R.string.drawer_aberto,R.string.drawer_fechad0)
-        {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-
+            public void onClick(View view) {
+                Snackbar.make(view, "Colocar algo aqui", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
+        });
+    }
 
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                super.onDrawerClosed(drawerView);
-
-                //pintar a antiga posicao ao normal
-                pintarItems(mUltimaPosicaoDoDrawer,false);
-
-                pintarItems(mPosicaoSelecionada,true);
-
-            }
-
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
-                super.onDrawerSlide(drawerView, slideOffset);
-
-            }
-        };
-
-
-        mDrawerLayout.setDrawerListener(mDrawerToogle);
-
-        //Definir os items da lista no adapter
-
-        NavigationDrawerListAdapter mDrawerAdapter = new NavigationDrawerListAdapter(this.getApplicationContext());
-
-        mDrawerAdapter.setDrawerHeader("Defina o link de uma imagem aqui para avatar","Gilson Gonçalves","Defina  o link para o backgroudn aqui");
-        mDrawerAdapter.addRow(R.drawable.ic_comingsoon,"SOBRE NÓS");
-        mDrawerAdapter.addRow(R.drawable.ic_ticket,"SERVIÇOS");
-        mDrawerAdapter.addRow(R.drawable.ic_settings,"CONTATO");
-
-        mListaDrawer.setAdapter(mDrawerAdapter);
-
-        mListaDrawer.setOnItemClickListener(new DrawerItemClickListener());
-    }*/
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -166,78 +125,4 @@ public class PrincipalActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-    private void setupDrawerContent(NavigationView navigationView) {
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        menuItem.setChecked(true);
-                        mDrawerLayout.closeDrawers();
-                        return true;
-                    }
-                });
-    }
-
-    public void pintarItems(int posicao, boolean isSelected) {
-        View view = mListaDrawer.getChildAt(posicao);
-
-        View mIconView = view.findViewById(R.id.row_icon);
-
-        Drawable iconDrawable = DrawableCompat.wrap(mIconView.getBackground());
-
-        TextView mRowTextView = (TextView) view.findViewById(R.id.drawer_row_line);
-
-        int color = getResources().getColor(R.color.black);
-
-        if (isSelected == true) {
-            color = getResources().getColor(R.color.primaryColor);
-        }
-
-        mRowTextView.setTextColor(color);
-
-        //muda a cor do drawable para as especificadas
-        DrawableCompat.setTint(iconDrawable, color);
-    }
-
-    /*private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView parent, View view, int position, long id) {
-
-            if (position != 0) {
-                mUltimaPosicaoDoDrawer = mPosicaoSelecionada;
-                mPosicaoSelecionada = position;
-                selectItem(position);
-            } else {
-                mDrawerLayout.closeDrawer(mListaDrawer);
-            }
-        }
-    }*/
-
-    /*public void selectItem(int position) {
-        //mViewPager.setCurrentItem(position);
-        FragmentManager fm = getSupportFragmentManager();
-
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        Fragment fr = null;
-        switch (position) {
-            case 1:
-                fr = new SobreNosFragment();
-                break;
-            case 2:
-                fr = new ServicoFragments();
-                break;
-            case 3:
-                fr = new ContatoFragment();
-                break;
-        }
-        //fragmentTransaction.replace(R.id.conteudo_layout, fr);
-        //fragmentTransaction.commit();
-
-        pintarItems(mUltimaPosicaoDoDrawer, false);
-
-        pintarItems(mPosicaoSelecionada, true);
-
-        mDrawerLayout.closeDrawer(mListaDrawer);
-    }*/
 }
---
+++ ApresentacaoAndroid/app/src/main/java/ggc/apresentaoandroid/ServicosActivity.java
@@ -0,0 +1,98 @@
+package ggc.apresentaoandroid;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servicos);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        final ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new AplicativoFragment(), getString(R.string.sub_aplicativo));
+        adapter.addFragment(new FabricaFragment(), getString(R.string.sub_fabrica));
+        adapter.addFragment(new SitesFragment(), getString(R.string.sub_sites));
+        viewPager.setAdapter(adapter);
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
+    }
+}
